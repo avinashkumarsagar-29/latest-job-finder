230239f80dd318a070e1a1286c79bbd60abfd973
avinashkumarsagar-29 <168345383+avinashkumarsagar-29@users.noreply.github.com>
2025-08-20 23:51:56 +0530

    url changed

--- backend/index.js
+++ backend/index.js
@@ -76,7 +76,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://latest-job-finder.onrender.com",
   credentials: true,
 };
 app.use(cors(corsOptions));
