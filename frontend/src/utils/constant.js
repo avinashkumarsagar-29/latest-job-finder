c53c17d13bf91389b563ccd0292451f9b066baf1
avinashkumarsagar-29 <168345383+avinashkumarsagar-29@users.noreply.github.com>
2025-08-24 19:10:49 +0530

    Fixed API endpoints for user, job, application, company

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -3,7 +3,11 @@
 // export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
 // export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
 
-export const USER_API_END_POINT="https://latest-job-finder.onrender.com";
-export const JOB_API_END_POINT="https://latest-job-finder.onrender.com";
-export const APPLICATION_API_END_POINT="https://latest-job-finder.onrender.com";
-export const COMPANY_API_END_POINT="https://latest-job-finder.onrender.com";
+// export const USER_API_END_POINT="https://latest-job-finder.onrender.com";
+// export const JOB_API_END_POINT="https://latest-job-finder.onrender.com";
+// export const APPLICATION_API_END_POINT="https://latest-job-finder.onrender.com";
+// export const COMPANY_API_END_POINT="https://latest-job-finder.onrender.com";
+export const USER_API_END_POINT = "https://latest-job-finder.onrender.com/api/v1/user";
+export const JOB_API_END_POINT = "https://latest-job-finder.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT = "https://latest-job-finder.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT = "https://latest-job-finder.onrender.com/api/v1/company";
