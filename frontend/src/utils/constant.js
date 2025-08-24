74efb6f9abaadf4a0ccea38e9301dfcd53f49ed4
avinashkumarsagar-29 <168345383+avinashkumarsagar-29@users.noreply.github.com>
2025-08-24 18:59:22 +0530

    Fix API endpoint typo (.comr -> .com)

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -3,7 +3,7 @@
 // export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
 // export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
 
-export const USER_API_END_POINT="https://latest-job-finder.onrender.comr";
+export const USER_API_END_POINT="https://latest-job-finder.onrender.com";
 export const JOB_API_END_POINT="https://latest-job-finder.onrender.com";
 export const APPLICATION_API_END_POINT="https://latest-job-finder.onrender.com";
 export const COMPANY_API_END_POINT="https://latest-job-finder.onrender.com";
